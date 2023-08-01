9e57b4b509d1143c748fad56050b73e4452e9caa
정지원 <panda981018@gmail.com>
2023-08-01 10:31:07 +0900

    코독 테스트

--- src/test/java/com/example/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/example/hellospring/service/MemberServiceIntegrationTest.java
@@ -23,7 +23,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         // given : 데이터
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring1");
 
         // when : 조건
         Long saveId = memberService.join(member);
